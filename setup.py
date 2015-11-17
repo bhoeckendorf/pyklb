c9d518fcbe29e0a82a45f75e31b526afab480292
Burkhard Hoeckendorf <b.hoeckendorf@web.de>
2015-11-16 20:41:00 -0500

    Handle build dependencies in setup.py

--- setup.py
+++ setup.py
@@ -2,22 +2,66 @@
 
 import os
 import numpy
+import urllib
+import platform
 from Cython.Build import cythonize
 from distutils.core import setup, Extension
 
-with open(os.path.abspath(os.path.join("..", "README.md")), "r") as f:
-    readme = f.read()
+
+# version (by commit id) of main library to use
+klbCommitId = "75c5eb72f91a"
+
+
+# downlad required headers
+downloadFiles = [
+    ("src/common.h", "include/common.h"),
+    ("src/klb_Cwrapper.h", "include/klb_Cwrapper.h")
+    ]
+for (source, target) in downloadFiles:
+    targetDir = os.path.abspath(
+        os.path.join("build", os.path.split(target)[0]) )
+    if not os.path.exists(targetDir):
+        os.makedirs(targetDir)
+    urllib.urlretrieve(
+        "https://bitbucket.org/fernandoamat/keller-lab-block-filetype/raw/%s/%s" % (klbCommitId, source),
+        "build/%s" % target)
+
+
+# download main library dependency
+errorMsg = """
+
+    *****************************************************************************++******
+    * No precompiled binary of main KLB library available.                              *
+    * Please download main KLB library source code from the link below and build it.    *
+    * https://bitbucket.org/fernandoamat/keller-lab-block-filetype/get/%s.zip *
+    *****************************************************************************++******
+
+    """ % klbCommitId
+downloadLib = None
+if platform.architecture()[0].startswith("64"):
+    platformName = platform.uname()[0].lower()
+    if "linux" in platformName:
+        downloadLib = "libklb.so"
+    elif "win" in platformName:
+        downloadLib = "klb.dll"
+    elif "mac" in platformName:
+        downloadLib = "libklb.dylib"
+if downloadLib == None:
+    print(errorMsg)
+else:
+    urllib.urlretrieve(
+        "https://bitbucket.org/fernandoamat/keller-lab-block-filetype/raw/%s/bin/%s" % (klbCommitId, downloadLib),
+        "pyklb/lib/%s" % downloadLib)
 
 setup(
     name = "pyklb",
     version = "0.0.1.dev",
-    description = readme.split("#")[2].split(".")[0].strip(), # from readme, use first sentence after header
-    long_description = readme,
-    url = "https://bitbucket.org/fernandoamat/keller-lab-block-filetype/",
+    description = "Python wrapper of the KLB file format, a high-performance file format for up to 5-dimensional arrays.",
+    long_description = "See https://bitbucket.org/fernandoamat/keller-lab-block-filetype",
+    url = "https://github.com/bhoeckendorf/pyklb",
     ext_modules = cythonize([
-        Extension("pyklb", ["pyklb.pyx"], libraries=["klb"])
-        ])
-    #py_modules = ["pyklb"],
-    #setup_requires = ["numpy"],
-    #install_requires = ["numpy"]
+        Extension("pyklb", ["pyklb/pyklb.pyx"], libraries=["klb"])
+        ]),
+    setup_requires = ["numpy"],
+    install_requires = ["numpy"]
 )
