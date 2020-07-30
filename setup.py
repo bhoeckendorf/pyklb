caba15d56c6cebc4e6c288a409eda6d9b382b960
Burkhard Hoeckendorf <burkhard.hoeckendorf@pm.me>
2020-07-29 23:58:20 -0500

    Add package to setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     description = "Python wrapper of the KLB file format, a high-performance file format for up to 5-dimensional arrays.",
     long_description = "See https://bitbucket.org/fernandoamat/keller-lab-block-filetype",
     url = "https://github.com/bhoeckendorf/pyklb",
+    packages=["pyklb"],
     setup_requires = ["numpy"],
     install_requires = ["cython", "numpy"]
 )
